3be2716d0a5b8c8460079822cd6a27052175abc7
NotPatch <nitradosamet@hotmail.com>
2025-08-02 05:24:43 +0300

    Added menu configuration

--- src/main/java/com/notpatch/nLeague/manager/ConfigurationManager.java
+++ src/main/java/com/notpatch/nLeague/manager/ConfigurationManager.java
@@ -2,6 +2,7 @@ package com.notpatch.nLeague.manager;
 
 import com.notpatch.nLeague.configuration.NConfiguration;
 import com.notpatch.nLeague.configuration.impl.LeagueConfiguration;
+import com.notpatch.nLeague.configuration.impl.MenuConfiguration;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -13,9 +14,11 @@ public class ConfigurationManager {
     private final List<NConfiguration> configurations = new ArrayList<>();
 
     private final LeagueConfiguration leagueConfiguration;
+    private final MenuConfiguration menuConfiguration;
 
     public ConfigurationManager(){
         configurations.add(leagueConfiguration = new LeagueConfiguration());
+        configurations.add(menuConfiguration = new MenuConfiguration());
     }
 
     public void loadConfigurations() {
